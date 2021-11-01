7761b4ed655221a61d64040070fbb3037b33de3e
Thiago Sequeira <thiagosequeira02@gmail.com>
2021-11-01 10:23:25 -0300

    adding colas

---
+++ Colas/ejercicio1.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int menu ();
+queue<int> load (queue<int>);
+void show (queue<int>);
+queue<int> delete_element (queue<int>);
+queue<int> insert (queue<int>);
+
+int main (){
+
+    int option;
+    queue<int> queue;
+
+    do{
+        option = menu();
+        switch(option){
+            case 0:
+                break;
+            case 1:
+                queue = load(queue);
+                break;
+            case 2:
+                show(queue);
+                break;
+            case 3:
+                queue = delete_element(queue);
+                break;
+            case 4:
+                queue = insert(queue);
+                break;
+            default:
+                cout<<"Ingrese una opcion valida"<<endl;
+        }
+    }while(option != 0);
+
+    return 0;
+}
+
+int menu (){
+
+    int option = 0;
+    cout<<"Ingrese la opcion a realizar"<<endl;
+    cout<<"1. Cargar datos"<<endl;
+    cout<<"2. Mostrar listado de datos"<<endl;
+    cout<<"3. Borrar un elemento de la cola"<<endl;
+    cout<<"4. Agregar un elemento a la cola"<<endl;
+    cout<<"0. Salir"<<endl;
+    cin>>option;
+    return option;
+
+}
+
+queue<int> load (queue<int> q){
+
+    int number;
+    cout<<"Ingrese los valores (O para salir)"<<endl;
+    do {
+        cin>>number;
+        if(number != 0){
+            q.push(number);
+        }
+
+    }while(number != 0);
+    return q;
+
+}
+
+void show (queue<int> q){
+
+    queue<int> aux;
+    while(!q.empty()){
+
+        cout<< q.front() <<" - ";
+        aux.push(q.front());
+        q.pop();
+
+    }
+    q = aux;
+    cout<<endl;
+
+}
+
+queue<int> delete_element (queue<int> q){
+
+    int quantity;
+
+    cout<<"Ingrese la cantidad de elementos a borrar"<<endl;
+    cin>>quantity;
+    for (int i = 0; i < quantity; ++i) {
+        q.pop();
+    }
+    return q;
+
+}
+
+queue<int> insert (queue<int> q){
+
+    int number;
+    cout<<"Ingrese el dato a agregar en la cola"<<endl;
+    cin>>number;
+    q.push(number);
+    return q;
+
+}
---
+++ Colas/ejercicio2.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct stock{
+	int codigo;
+	string nombre;
+	int cantidad;
+	int precioLista;
+};
+
+void mostrar (queue<stock>);
+queue <stock> descontar(queue<stock>);
+queue <stock> reponer(queue<stock>);
+
+int menu ();
+
+int main (){
+	
+	struct stock s;
+	queue<stock> colaS;
+	
+	s.codigo = 1;
+	s.nombre = "Leche";
+	s.cantidad = 23;
+	s.precioLista = 210;
+	colaS.push(s);
+	
+	s.codigo = 2;
+	s.nombre = "Choco";
+	s.cantidad = 42;
+	s.precioLista = 523;
+	colaS.push(s);
+	
+	s.codigo = 3;
+	s.nombre = "Galle";
+	s.cantidad = 3;
+	s.precioLista = 120;
+	colaS.push(s);
+	
+	int option;
+	
+	do{
+		option = menu();
+		switch(option){
+		case 1:
+			mostrar(colaS);
+			break;
+		case 2:
+			colaS = descontar(colaS);
+			break;
+		case 3:
+			colaS = reponer(colaS);
+			break;
+		default:
+			cout<<"Ingrese una opcion valida"<<endl;
+		}
+	}while(option != 0);
+	
+	return 0;
+}
+	
+int menu (){
+	
+	int option = 0;
+	
+	cout<<"Ingrese la opcion a realizar"<<endl;
+	cout<<"1. Mostrar listado de datos"<<endl;
+	cout<<"2. Borrar un elemento de la cola"<<endl;
+	cout<<"3. Agregar un elemento a la cola"<<endl;
+	cout<<"0. Salir"<<endl;
+	cin>>option;
+	
+	return option;	
+}
+	
+void mostrar (queue<stock> colaS){
+	struct stock s;
+	
+	queue<stock> auxCola;
+	cout << "Cod\tNom\tCant\tPre" << endl;
+	while(!colaS.empty()){
+		s = colaS.front();
+		cout << s.codigo << "\t" << s.nombre << "\t" << s.cantidad << "\t" << s.precioLista << endl;
+		auxCola.push(colaS.front());
+		colaS.pop();
+	}
+	colaS = auxCola;
+}
+	
+queue <stock> descontar(queue<stock> colaS){
+	struct stock s;
+	queue<stock> auxCola;
+	int cod = 0, desc = 0;
+	cout << "ingrese el codigo del producto a descontar: ";
+	cin >> cod;
+
+	while(!colaS.empty()){
+		s = colaS.front();
+		if(s.codigo == cod){
+			cout << "hay " << s.cantidad << " productos de " << s.nombre << ". ingrese la cantidad a descontar: ";
+			cin >> desc;
+			while(desc > s.cantidad){
+				cout << "ingrese una cantidad menor o igual a " << s.nombre << ": ";
+				cin >> desc;
+			}
+			s.cantidad = s.cantidad - desc;
+		}
+		auxCola.push(s);
+		colaS.pop();
+	}
+	return auxCola;
+}
+	
+queue <stock> reponer(queue<stock> colaS){
+	struct stock s;
+	queue<stock> auxCola;
+	int cod = 0, rep = 0;
+	cout << "ingrese el codigo del producto a reponer: ";
+	cin >> cod;
+	
+	while(!colaS.empty()){
+		s = colaS.front();
+		if(s.codigo == cod){
+			cout << "hay " << s.cantidad << " productos de " << s.nombre << ". ingrese la cantidad a descontar: ";
+			cin >> rep;
+			while(rep <= 0){
+				cout << "ingrese una cantidad mayor a cero: ";
+				cin >> rep;
+			}
+			s.cantidad = s.cantidad + rep;
+		}
+		auxCola.push(s);
+		colaS.pop();
+	}
+	return auxCola;
+}
